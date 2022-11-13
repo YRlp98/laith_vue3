02b6acc9c5d5b82832c173bd3a7c02e8cd978632
yrlp98 <Yousef.r1999@gmail.com>
2022-11-13 17:25:33 +0330

    vue_counter_app2

---
+++ vue_counter_app2/src/App.vue
@@ -0,0 +1,62 @@
+<!--! Composition API -->
+<!-- <script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const addToCount = () => {
+  count.value = count.value + 1;
+};
+
+const subtractFromCount = () => {
+  count.value = count.value - 1;
+};
+</script> -->
+<!--! Composition API -->
+
+<!--! Options API -->
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    addToCount() {
+      this.count = this.count + 1;
+    },
+    subtractFromCount() {
+      this.count = this.count - 1;
+    },
+  },
+};
+</script>
+<!--! Options API -->
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractFromCount()">-</button>
+      <button @click="addToCount()">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  color: black;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
